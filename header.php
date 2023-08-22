f68e4b211b0a51e8323e41bd7e8605b65f75037c
lotestcode <nikhilchandradas.v2@gmail.com>
2023-08-22 20:45:15 +0600

    header.php

---
+++ header.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="./img/main/nb.png" type="image/png">
+        <title>Nulls Brawl Download</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="vendors/flaticon/flaticon.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body>
+    
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu" id="mainNav">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container box_1620">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.html"><img style="height: 50px;" src="img/main/logo-2.png" alt="Nulls brawl"></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
+								
+									<li class="nav-item submenu dropdown">
+										<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+										<ul class="dropdown-menu">
+											<li class="nav-item"><a class="nav-link" href="/">Null's Brawl APK</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Null's Brawl IOS (coming soon)</a></li>
+											
+										</ul>
+									</li> 
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="About.php">About</a></li>
+										<li class="nav-item"><a class="nav-link" href="Disclaimer.php">Disclaimer</a></li>
+										<li class="nav-item"><a class="nav-link" href="Terms-and-Conditions.php">TOS</a></li>
+										<li class="nav-item"><a class="nav-link" href="Privacy-Policy.php">Privacy Policy</a></li>
+										<li class="nav-item"><a class="nav-link" href="Contact.php">Contact</a></li>
+									</ul>
+								</li> 
+								<li class="nav-item"><a class="nav-link" href="#">Blog (coming soon)</a></li>
+							</ul>
+						</div> 
+					</div>
+            	</nav>
+            </div>
+        </header>
