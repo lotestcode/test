b8a5df36cdffd2dd4e9089a2133c143c66afc5cd
lotestcode <nikhilchandradas.v2@gmail.com>
2023-08-24 15:49:03 +0600

    update 404 page

--- 404.php
+++ 404.php
@@ -1,42 +1,39 @@
-<?php
-include("header.php");
-?>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="https:/dl.nulls-brawl-download.com/img/main/nb.png" type="image/png">
+        <title>404</title>
 <style>
 .fulll{
     background-color: #f1f1f1;
+    text-align: center;
 }
 .vh{
-    height: 100vh;
+    height: 97vh;
 }
 .mai{
-    padding-top: 50vh;
+    padding-top: 30vh;
 }
 .ccss{
     font-size: 20px;
+}
+.top{
+    margin-bottom: 30px;
+    font-size: 40px;
+}
+.btn{
+    background-color: blue;
+    padding: 10px 25px;
+    border-radius: 5px;
+    text-decoration: none;
+    color: #fff;
 }
 </style>
 <div class="fulll">
 <div class="container text-center vh">
     <div class="mai">
-        <h2>404<br>Page not found</h2>
-        <a class="ccss btn btn-primary shadow mt-3" href="./"><i class="lnr lnr-home ccss"></i> Go To Home</a>
+    <a class="navbar-brand logo_h" href="https:/dl.nulls-brawl-download.com"><img style="height: 50px;" src="https:/dl.nulls-brawl-download.com/img/main/logo-2.png" alt="Nulls brawl"></a>
+        <h2 class="top">404<br>Page not found</h2>
+        <a class="ccss btn btn-primary shadow mt-3" href="https:/dl.nulls-brawl-download.com"><i class="lnr lnr-home ccss"></i> Go To Home</a>
     </div>
 </div>
 </div>
-<script>
-    const express = require('express');
-const app = express();
-
-// ... other routes and middleware ...
-
-// 404 Error Handling
-app.use(function(req, res, next) {
-    res.status(404).sendFile(__dirname + '/404.php');
-});
-
-// ... start the server ...
-
-</script>
-<?php
-include("footer.php");
-?>
