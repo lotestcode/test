c34ae0a5fd0bc2fb2dbd1b06f3a9a5145918ff8d
lotestcode <nikhilchandradas.v2@gmail.com>
2023-08-24 15:56:07 +0600

    404

--- 404.php
+++ 404.php
@@ -31,7 +31,7 @@
 <div class="fulll">
 <div class="container text-center vh">
     <div class="mai">
-    <a class="navbar-brand logo_h" href="https://dl.nulls-brawl-download.com"><img style="height: 50px;" src="https:dl.nulls-brawl-download.com/img/main/logo-2.png" alt="Nulls brawl"></a>
+    <a class="navbar-brand logo_h" href="https://dl.nulls-brawl-download.com"><img style="height: 50px;" src="https://dl.nulls-brawl-download.com/img/main/logo-2.png" alt="Nulls brawl"></a>
         <h2 class="top">404<br>Page not found</h2>
         <a class="ccss btn btn-primary shadow mt-3" href="https://dl.nulls-brawl-download.com"><i class="lnr lnr-home ccss"></i> Go To Home</a>
     </div>
